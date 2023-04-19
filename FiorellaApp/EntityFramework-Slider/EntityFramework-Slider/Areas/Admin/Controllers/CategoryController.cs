ee277bc51cbd0eec6d96ce27440971f379f8b1c2
Konul Ibrahimova <konulsi@code.edu.az>
2023-04-19 17:05:00 +0400

    Task are ready

--- FiorellaApp/EntityFramework-Slider/EntityFramework-Slider/Areas/Admin/Controllers/CategoryController.cs
+++ FiorellaApp/EntityFramework-Slider/EntityFramework-Slider/Areas/Admin/Controllers/CategoryController.cs
@@ -2,6 +2,7 @@
 using EntityFramework_Slider.Models;
 using EntityFramework_Slider.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EntityFramework_Slider.Areas.Admin.Controllers
 {
@@ -23,7 +24,7 @@ namespace EntityFramework_Slider.Areas.Admin.Controllers
 
 
         [HttpGet]
-        public IActionResult Create()     /*async-elemirik cunku data gelmir databazadan*/
+        public IActionResult Create()     /*async-elemirik cunku data gelmir databazadan*/ //sadece indexe gedir hansiki inputa data elavve edib category yaradacaq
         {
 
             return View();
@@ -34,11 +35,50 @@ namespace EntityFramework_Slider.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Category category)     // bazaya neyise save edirik deye asinxron olmalidi
         {
-            await _context.Categories.AddAsync(category);  //bazadaki categorie tablesine category ni add edir liste
-            await _context.SaveChangesAsync();    //save edir bazaya 
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                if (!ModelState.IsValid)    // create eden zaman input null olarsa, yeni isvalid deyilse(isvalid olduqda data daxil edilir) view a qayit
+                {
+                    return View();
+                }
+
+                var existData = await _context.Categories.FirstOrDefaultAsync(m => m.Name.Trim().ToLower() == category.Name.Trim().ToLower());
+                // yoxlayiriq bize gelen yeni inputa yazilan name databazada varsa  error chixartmaq uchun
+
+                if (existData is not null) /*gelen data databazamizda varsa yeni null deyilse*/
+                {
+                    ModelState.AddModelError("Name", "This data already exist!"); // bu inputa name daxil etmeyende error chixartsin. Name propertisinin altinda. buradaki Name hemin input-un adidir.
+
+                    return View();
+                }
+
+                int num = 1;
+                int num2 = 0;
+                int result = num / num2;
+
+                await _context.Categories.AddAsync(category);  //bazadaki categorie tablesine category ni add edir liste
+                await _context.SaveChangesAsync();    //save edir bazaya 
+                return RedirectToAction(nameof(Index));
+
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.error = ex.Message;
+                return View();
+            }
+           
         }
 
 
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+
+
+
+            
     }
 }
--- FiorellaApp/EntityFramework-Slider/EntityFramework-Slider/Models/Category.cs
+++ FiorellaApp/EntityFramework-Slider/EntityFramework-Slider/Models/Category.cs
@@ -1,8 +1,13 @@
-﻿namespace EntityFramework_Slider.Models
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityFramework_Slider.Models
 {
     public class Category:BaseEntity
     {
-        public string? Name { get; set; }
-        public ICollection<Product> Products { get; set; }
+        [Required] //null olub olmamasini yoxlayir. bu atributu qoyuruqsa null gele bilmez. yeni category create edende name vacib yazilmalidir.
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }  //categoryden yola chixanda producta chata bilmek uchun
     }
 }
